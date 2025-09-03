32c731877b6b653c03578b5e30de7e5ad06bddb1
Talha Bashir <contact@talhacodes.com>
2025-09-03 23:53:54 +0500

    commit

---
+++ component/FaqComp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FaQuestion } from "react-icons/fa6";
+import { LuArrowDown } from "react-icons/lu";
+
+function FaqComp() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FaqComp
--- pages/SingleProduct.jsx
+++ pages/SingleProduct.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import tape from "../src/assets/icon.tape_measure.svg"
+import heart from "../src/assets/icon.heart.svg"
+import truck from "../src/assets/icon.truck.svg"
+import retusn from '../src/assets/icon.return.svg'
 
 function SingleProduct() {
     const [images,setImages] = React.useState({
@@ -39,10 +43,32 @@ function SingleProduct() {
             </div>
         </div>
         <div>
-            <div>
+            <div className='py-10'>
                 <h1 className='text-4xl leading-12 mb-2'>V-neck cotton T-shirt</h1>
                 <div className="reviews">⭐⭐⭐⭐⭐<span className='text-[#4d4e4f]'>(134 reviews)</span></div>
                 <p className='text-3xl font-semibold my-5'>$19.00</p>
+                <div>
+                     <h2 className='mb-4'>Colors: <span>Grey</span></h2>
+                     <div className='flex flex-row gap-2'>
+                        <button className='bg-amber-600 shadow-2xl border border-[#4d4e4f] rounded-full p-4'></button>
+                        <button className='bg-black shadow-2xl border border-[#4d4e4f] rounded-full p-4'></button>
+                        <button className='bg-blue-900 shadow-2xl border border-[#4d4e4f] rounded-full p-4'></button>
+                        <button className='bg-red-950 shadow-2xl border border-[#4d4e4f] rounded-full p-4'></button>
+                        <button className='bg-white shadow-2xl border border-[#4d4e4f] rounded-full p-4'></button>
+                        </div>
+                   
+
+                </div>
+                <p className='text-lg mt-3 text-[#5d8fc1] font-light'>Alert me when available</p>
+                <div className='my-10 flex flex-col gap-4'>
+                     <div className='flex gap-2 text-[#4d4e4f]'><img src={tape} alt="" /> <span> 20 x 67" (53 x 170cm)</span></div>
+                <div className='flex gap-2 text-[#4d4e4f]'><img src={truck} alt="" /> <span>Free and express US shipping </span><span className='text-[#5d8fc1]'>See details</span></div>
+                <div className='flex gap-2 text-[#4d4e4f]'><img src={retusn} alt="" /> <span>Free 14 day US returns </span><span className='text-[#5d8fc1]'>See details</span></div>
+                <div className='flex gap-2 text-[#4d4e4f]'><img src={heart} alt="" /> <span>100% Handmade in Italy</span></div>
+
+                </div>
+               
+               
                 
             </div>
             
